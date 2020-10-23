964f9ef926088e5e0a78b7afc62b21cb28b17334
rising4ce <rising4ce@ya.ru>
2020-10-23 14:51:25 +0300

    improving camera

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -1,23 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 public class FlyCamera : MonoBehaviour
 {
-
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-
-
-    float mainSpeed = 25.0f; //regular speed
-    float shiftAdd = 50.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 200.0f; //Maximum speed when holdin gshift
-    float camSens = 0.2f; //How sensitive it with mouse
+    [SerializeField]
+    private readonly float mainSpeed = 25.0f; //regular speed
+    [SerializeField]
+    private readonly float shiftAdd = 50.0f; //multiplied by how long shift is held.  Basically running
+    [SerializeField]
+    private readonly float maxShift = 200.0f; //Maximum speed when holdin gshift
+    [SerializeField]
+    private readonly float camSens = 0.2f; //How sensitive it with mouse
+    [SerializeField]
+    private readonly float verticalSpeed = 20.0f;
     private Vector3 lastMouse = new Vector3(0,0,0); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
 
@@ -28,20 +24,22 @@ public class FlyCamera : MonoBehaviour
 
     void Update()
     {
+        // Rotation
         lastMouse = Input.mousePosition - lastMouse;
         lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
         lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
+        if (Input.GetMouseButton(0))
+        {
+            transform.eulerAngles = lastMouse;
+        }
         lastMouse = Input.mousePosition;
-        //Mouse  camera angle done.  
 
-        //Keyboard commands
-        //float f = 0.0f;
+        // Movement
         Vector3 p = GetBaseInput();
         if (Input.GetKey(KeyCode.LeftShift))
         {
             totalRun += Time.deltaTime;
-            p = p * totalRun * shiftAdd;
+            p *= totalRun * shiftAdd;
             p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
             p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
             p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
@@ -49,44 +47,41 @@ public class FlyCamera : MonoBehaviour
         else
         {
             totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
+            p *= mainSpeed;
         }
-
-        p = p * Time.deltaTime;
+        p *= Time.deltaTime;
         Vector3 newPosition = transform.position;
-        if (Input.GetKey(KeyCode.Space))
-        { //If player wants to move on X and Z axis only
-            transform.Translate(p);
-            newPosition.x = transform.position.x;
-            newPosition.z = transform.position.z;
-            transform.position = newPosition;
-        }
-        else
-        {
-            transform.Translate(p);
-        }
 
+        // Position set
+        transform.Translate(p);
+        newPosition.x = transform.position.x;
+        newPosition.z = transform.position.z;
+        var verticalDelta = verticalSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+            newPosition.y += verticalDelta;
+        if (Input.GetKey(KeyCode.LeftControl))
+            newPosition.y -= verticalDelta;
+        transform.position = newPosition;
     }
 
+    /*
+     * [DllImport("User32.Dll")]
+     * public static extern long SetCursorPos(int x, int y);
+     * 
+     * better camera will be implemented in next version
+    */
+
     private Vector3 GetBaseInput()
-    { //returns the basic values, if it's 0 than it's not active.
+    { 
         Vector3 p_Velocity = new Vector3();
         if (Input.GetKey(KeyCode.W))
-        {
             p_Velocity += new Vector3(0, 0, 1);
-        }
         if (Input.GetKey(KeyCode.S))
-        {
             p_Velocity += new Vector3(0, 0, -1);
-        }
         if (Input.GetKey(KeyCode.A))
-        {
             p_Velocity += new Vector3(-1, 0, 0);
-        }
         if (Input.GetKey(KeyCode.D))
-        {
             p_Velocity += new Vector3(1, 0, 0);
-        }
         return p_Velocity;
     }
 }
