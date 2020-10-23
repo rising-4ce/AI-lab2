0687467f07c0a01d111fa3cb1594e1376c6dbac2
rising4ce <rising4ce@ya.ru>
2020-10-23 20:48:26 +0300

    proper platform physics

---
+++ Assets/Scripts/AttachBot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public GameObject Bot;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Bot)
+            Bot.transform.parent = transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Bot)
+            Bot.transform.parent = null;
+    }
+}
